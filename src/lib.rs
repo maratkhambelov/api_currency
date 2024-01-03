ee48c3122c8832dfd98a80bdbbc6341cfb99bf4f
MaratKhambelov <maratkhambelov@gmail.com>
2024-01-03 10:56:09 +0300

    version 1

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,6 @@ pub mod schema;
 use diesel::prelude::*;
 use dotenvy::dotenv;
 use std::env;
-use crate::models::{NewRate, Rate};
 
 pub fn add(left: usize, right: usize) -> usize {
     left + right
@@ -28,17 +27,4 @@ pub fn establish_connection() -> SqliteConnection {
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     SqliteConnection::establish(&database_url)
         .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
-}
-
-//TODO: delete
-pub fn create_rate(conn: &mut SqliteConnection, base_currency: &str, target_currency: &str, last_update: &str) -> Rate {
-    use crate::schema::exchanges_rates;
-
-    let new_rate = NewRate { base_currency, target_currency, rate: 1f32, last_update };
-
-    diesel::insert_into(exchanges_rates::table)
-        .values(&new_rate)
-        .returning(Rate::as_returning())
-        .get_result(conn)
-        .expect("Error saving new rate")
 }
